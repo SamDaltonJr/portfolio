c5920cc7803f3f884a25ab45f6d8900806d84659
Sam Dalton <spd4nf@virginia.edu>
2019-02-06 00:59:05 -0600

    added spacer and fixed centering on images

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,138 +4,143 @@ import './HomePage.css';
 
 export default function HomePage() {
   return (
-    <main>
-      <section id="projects">
-        <h2>My Projects</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
-          tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
-          pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
-          orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa erat.
-          Duis efficitur, purus eu posuere porttitor, elit massa dignissim nisi,
-          vel auctor purus arcu sed mi. In mollis aliquam porttitor. Integer
-          molestie felis sem, sit amet ultrices ipsum vehicula in. Aliquam porta
-          urna malesuada justo porttitor rutrum. Phasellus ultricies risus nec
-          elit egestas facilisis. Quisque ac ligula turpis. Maecenas scelerisque
-          tellus vel blandit placerat.
-        </p>
-        <p>
-          Mauris libero neque, bibendum et lorem ut, eleifend fermentum dolor.
-          Praesent ac dui sed nunc lacinia consequat ut ac purus. Aliquam quis
-          mattis nibh, ut porttitor ex. Maecenas ut tellus sit amet velit
-          euismod lobortis eu ac metus. Sed id libero eu augue convallis semper
-          nec vel magna. Aliquam volutpat cursus rutrum. Phasellus et augue et
-          turpis mattis ultrices. Nullam sit amet odio nulla. Duis sit amet
-          sapien pellentesque, pharetra erat eget, aliquet lectus. Pellentesque
-          eu massa eleifend, ullamcorper ligula id, vulputate mi. Nullam at sem
-          a magna tincidunt blandit vel eu ante. Ut porta vel libero et viverra.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Vivamus cursus orci
-          non tortor vestibulum consectetur sed sit amet odio. Duis accumsan
-          lorem id lorem tincidunt porttitor.
-        </p>
-        <p>
-          Fusce vehicula convallis sagittis. Suspendisse finibus tincidunt lorem
-          sed sagittis. Fusce vestibulum odio non euismod euismod. Donec ut ante
-          sodales, pretium nunc ut, pharetra augue. Proin a mi ac leo semper
-          aliquam eu a augue. Sed accumsan, velit non tempor accumsan, quam mi
-          fringilla lectus, sit amet fermentum erat urna nec lacus. Cras
-          vehicula nisi in neque consectetur, at dapibus metus tempor. Integer
-          ultrices posuere venenatis. Etiam sed aliquet leo. Fusce feugiat nibh
-          eu libero egestas malesuada.
-        </p>
-        <p>
-          Aliquam fringilla purus lectus, eget elementum ex laoreet id. Donec
-          faucibus dolor eget aliquam mollis. Aenean ullamcorper posuere
-          convallis. Quisque ac diam ac ligula dapibus imperdiet id nec urna. Ut
-          libero nulla, rutrum ac purus in, mattis iaculis mauris. Nam justo
-          urna, tincidunt ut nibh nec, vehicula aliquet elit. Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Proin elementum viverra
-          iaculis. Phasellus vitae mi accumsan, mollis nunc vel, mollis nibh.
-          Nunc ullamcorper efficitur mi ut finibus. Donec faucibus nisl massa,
-          vel sollicitudin sem vehicula dignissim. Proin egestas ipsum nibh, sed
-          bibendum turpis gravida at. Donec mattis rutrum pretium. Praesent
-          ullamcorper mollis nisi sit amet ornare.
-        </p>
-      </section>
-      <section id="about">
-        <h2>About me</h2>
-        <p>This is a paragraph about me.</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
-          tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
-          pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
-          orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa erat.
-          Duis efficitur, purus eu posuere porttitor, elit massa dignissim nisi,
-          vel auctor purus arcu sed mi. In mollis aliquam porttitor. Integer
-          molestie felis sem, sit amet ultrices ipsum vehicula in. Aliquam porta
-          urna malesuada justo porttitor rutrum. Phasellus ultricies risus nec
-          elit egestas facilisis. Quisque ac ligula turpis. Maecenas scelerisque
-          tellus vel blandit placerat.
-        </p>
-        <p>
-          Mauris libero neque, bibendum et lorem ut, eleifend fermentum dolor.
-          Praesent ac dui sed nunc lacinia consequat ut ac purus. Aliquam quis
-          mattis nibh, ut porttitor ex. Maecenas ut tellus sit amet velit
-          euismod lobortis eu ac metus. Sed id libero eu augue convallis semper
-          nec vel magna. Aliquam volutpat cursus rutrum. Phasellus et augue et
-          turpis mattis ultrices. Nullam sit amet odio nulla. Duis sit amet
-          sapien pellentesque, pharetra erat eget, aliquet lectus. Pellentesque
-          eu massa eleifend, ullamcorper ligula id, vulputate mi. Nullam at sem
-          a magna tincidunt blandit vel eu ante. Ut porta vel libero et viverra.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Vivamus cursus orci
-          non tortor vestibulum consectetur sed sit amet odio. Duis accumsan
-          lorem id lorem tincidunt porttitor.
-        </p>
-        <p>
-          Fusce vehicula convallis sagittis. Suspendisse finibus tincidunt lorem
-          sed sagittis. Fusce vestibulum odio non euismod euismod. Donec ut ante
-          sodales, pretium nunc ut, pharetra augue. Proin a mi ac leo semper
-          aliquam eu a augue. Sed accumsan, velit non tempor accumsan, quam mi
-          fringilla lectus, sit amet fermentum erat urna nec lacus. Cras
-          vehicula nisi in neque consectetur, at dapibus metus tempor. Integer
-          ultrices posuere venenatis. Etiam sed aliquet leo. Fusce feugiat nibh
-          eu libero egestas malesuada.
-        </p>
-        <p>
-          Aliquam fringilla purus lectus, eget elementum ex laoreet id. Donec
-          faucibus dolor eget aliquam mollis. Aenean ullamcorper posuere
-          convallis. Quisque ac diam ac ligula dapibus imperdiet id nec urna. Ut
-          libero nulla, rutrum ac purus in, mattis iaculis mauris. Nam justo
-          urna, tincidunt ut nibh nec, vehicula aliquet elit. Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Proin elementum viverra
-          iaculis. Phasellus vitae mi accumsan, mollis nunc vel, mollis nibh.
-          Nunc ullamcorper efficitur mi ut finibus. Donec faucibus nisl massa,
-          vel sollicitudin sem vehicula dignissim. Proin egestas ipsum nibh, sed
-          bibendum turpis gravida at. Donec mattis rutrum pretium. Praesent
-          ullamcorper mollis nisi sit amet ornare.
-        </p>
-      </section>
-      <section id="contact">
-        <h2>Contact Me</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
-          tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
-          pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
-          orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa erat.
-          Duis efficitur, purus eu posuere porttitor, elit massa dignissim nisi,
-          vel auctor purus arcu sed mi. In mollis aliquam porttitor. Integer
-          molestie felis sem, sit amet ultrices ipsum vehicula in. Aliquam porta
-          urna malesuada justo porttitor rutrum. Phasellus ultricies risus nec
-          elit egestas facilisis. Quisque ac ligula turpis. Maecenas scelerisque
-          tellus vel blandit placerat.
-        </p>
-      </section>
-    </main>
+    <React.Fragment>
+      <header>
+        <div className="space-eater" />
+      </header>
+      <main>
+        <section id="projects">
+          <h2>My Projects</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
+            tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Curabitur
+            placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
+            pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
+            orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa
+            erat. Duis efficitur, purus eu posuere porttitor, elit massa
+            dignissim nisi, vel auctor purus arcu sed mi. In mollis aliquam
+            porttitor. Integer molestie felis sem, sit amet ultrices ipsum
+            vehicula in. Aliquam porta urna malesuada justo porttitor rutrum.
+            Phasellus ultricies risus nec elit egestas facilisis. Quisque ac
+            ligula turpis. Maecenas scelerisque tellus vel blandit placerat.
+          </p>
+          <p>
+            Mauris libero neque, bibendum et lorem ut, eleifend fermentum dolor.
+            Praesent ac dui sed nunc lacinia consequat ut ac purus. Aliquam quis
+            mattis nibh, ut porttitor ex. Maecenas ut tellus sit amet velit
+            euismod lobortis eu ac metus. Sed id libero eu augue convallis
+            semper nec vel magna. Aliquam volutpat cursus rutrum. Phasellus et
+            augue et turpis mattis ultrices. Nullam sit amet odio nulla. Duis
+            sit amet sapien pellentesque, pharetra erat eget, aliquet lectus.
+            Pellentesque eu massa eleifend, ullamcorper ligula id, vulputate mi.
+            Nullam at sem a magna tincidunt blandit vel eu ante. Ut porta vel
+            libero et viverra. Interdum et malesuada fames ac ante ipsum primis
+            in faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Vivamus cursus orci non tortor vestibulum consectetur sed sit
+            amet odio. Duis accumsan lorem id lorem tincidunt porttitor.
+          </p>
+          <p>
+            Fusce vehicula convallis sagittis. Suspendisse finibus tincidunt
+            lorem sed sagittis. Fusce vestibulum odio non euismod euismod. Donec
+            ut ante sodales, pretium nunc ut, pharetra augue. Proin a mi ac leo
+            semper aliquam eu a augue. Sed accumsan, velit non tempor accumsan,
+            quam mi fringilla lectus, sit amet fermentum erat urna nec lacus.
+            Cras vehicula nisi in neque consectetur, at dapibus metus tempor.
+            Integer ultrices posuere venenatis. Etiam sed aliquet leo. Fusce
+            feugiat nibh eu libero egestas malesuada.
+          </p>
+          <p>
+            Aliquam fringilla purus lectus, eget elementum ex laoreet id. Donec
+            faucibus dolor eget aliquam mollis. Aenean ullamcorper posuere
+            convallis. Quisque ac diam ac ligula dapibus imperdiet id nec urna.
+            Ut libero nulla, rutrum ac purus in, mattis iaculis mauris. Nam
+            justo urna, tincidunt ut nibh nec, vehicula aliquet elit. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum
+            viverra iaculis. Phasellus vitae mi accumsan, mollis nunc vel,
+            mollis nibh. Nunc ullamcorper efficitur mi ut finibus. Donec
+            faucibus nisl massa, vel sollicitudin sem vehicula dignissim. Proin
+            egestas ipsum nibh, sed bibendum turpis gravida at. Donec mattis
+            rutrum pretium. Praesent ullamcorper mollis nisi sit amet ornare.
+          </p>
+        </section>
+        <section id="about">
+          <h2>About me</h2>
+          <p>This is a paragraph about me.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
+            tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Curabitur
+            placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
+            pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
+            orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa
+            erat. Duis efficitur, purus eu posuere porttitor, elit massa
+            dignissim nisi, vel auctor purus arcu sed mi. In mollis aliquam
+            porttitor. Integer molestie felis sem, sit amet ultrices ipsum
+            vehicula in. Aliquam porta urna malesuada justo porttitor rutrum.
+            Phasellus ultricies risus nec elit egestas facilisis. Quisque ac
+            ligula turpis. Maecenas scelerisque tellus vel blandit placerat.
+          </p>
+          <p>
+            Mauris libero neque, bibendum et lorem ut, eleifend fermentum dolor.
+            Praesent ac dui sed nunc lacinia consequat ut ac purus. Aliquam quis
+            mattis nibh, ut porttitor ex. Maecenas ut tellus sit amet velit
+            euismod lobortis eu ac metus. Sed id libero eu augue convallis
+            semper nec vel magna. Aliquam volutpat cursus rutrum. Phasellus et
+            augue et turpis mattis ultrices. Nullam sit amet odio nulla. Duis
+            sit amet sapien pellentesque, pharetra erat eget, aliquet lectus.
+            Pellentesque eu massa eleifend, ullamcorper ligula id, vulputate mi.
+            Nullam at sem a magna tincidunt blandit vel eu ante. Ut porta vel
+            libero et viverra. Interdum et malesuada fames ac ante ipsum primis
+            in faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Vivamus cursus orci non tortor vestibulum consectetur sed sit
+            amet odio. Duis accumsan lorem id lorem tincidunt porttitor.
+          </p>
+          <p>
+            Fusce vehicula convallis sagittis. Suspendisse finibus tincidunt
+            lorem sed sagittis. Fusce vestibulum odio non euismod euismod. Donec
+            ut ante sodales, pretium nunc ut, pharetra augue. Proin a mi ac leo
+            semper aliquam eu a augue. Sed accumsan, velit non tempor accumsan,
+            quam mi fringilla lectus, sit amet fermentum erat urna nec lacus.
+            Cras vehicula nisi in neque consectetur, at dapibus metus tempor.
+            Integer ultrices posuere venenatis. Etiam sed aliquet leo. Fusce
+            feugiat nibh eu libero egestas malesuada.
+          </p>
+          <p>
+            Aliquam fringilla purus lectus, eget elementum ex laoreet id. Donec
+            faucibus dolor eget aliquam mollis. Aenean ullamcorper posuere
+            convallis. Quisque ac diam ac ligula dapibus imperdiet id nec urna.
+            Ut libero nulla, rutrum ac purus in, mattis iaculis mauris. Nam
+            justo urna, tincidunt ut nibh nec, vehicula aliquet elit. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum
+            viverra iaculis. Phasellus vitae mi accumsan, mollis nunc vel,
+            mollis nibh. Nunc ullamcorper efficitur mi ut finibus. Donec
+            faucibus nisl massa, vel sollicitudin sem vehicula dignissim. Proin
+            egestas ipsum nibh, sed bibendum turpis gravida at. Donec mattis
+            rutrum pretium. Praesent ullamcorper mollis nisi sit amet ornare.
+          </p>
+        </section>
+        <section id="contact">
+          <h2>Contact Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            consectetur nulla tortor, vitae dignissim diam imperdiet eu. Donec
+            tincidunt ut nisl ac iaculis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Curabitur
+            placerat eget justo sit amet laoreet. Nunc semper, lectus vitae
+            pulvinar vehicula, dui diam accumsan massa, sit amet pharetra diam
+            orci ac arcu. In blandit hendrerit elit a luctus. Sed sed massa
+            erat. Duis efficitur, purus eu posuere porttitor, elit massa
+            dignissim nisi, vel auctor purus arcu sed mi. In mollis aliquam
+            porttitor. Integer molestie felis sem, sit amet ultrices ipsum
+            vehicula in. Aliquam porta urna malesuada justo porttitor rutrum.
+            Phasellus ultricies risus nec elit egestas facilisis. Quisque ac
+            ligula turpis. Maecenas scelerisque tellus vel blandit placerat.
+          </p>
+        </section>
+      </main>
+    </React.Fragment>
   );
 }
