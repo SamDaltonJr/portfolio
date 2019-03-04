5099b7384a073a490b599097d55d1af1e9d6b5e4
Sam Dalton <spd4nf@virginia.edu>
2019-03-04 13:19:42 -0600

    added FO76 project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,6 +14,12 @@ import lists from '../../images/Gocery/lists-page.png';
 import items from '../../images/Gocery/items-page.png';
 import newlist from '../../images/Gocery/create-new-list.png';
 
+//inventory manager
+import fo76dashboard from '../../images/FO76/dashboard.png';
+import fo76add from '../../images/FO76/add-item.png';
+import fo76view from '../../images/FO76/view-item.png';
+import fo76edit from '../../images/FO76/edit-item.png';
+
 import Carousel from '../Carousel';
 
 export default class Projects extends React.Component {
@@ -44,7 +50,7 @@ export default class Projects extends React.Component {
             <section className="hero">
               <img src={gocerylanding} alt="Gocery Landing Page" />
               <div className="hero-titles">
-                <h3>Welcome to Gocery</h3>
+                <h3>Welcome to GoCery</h3>
                 <p>
                   Spend less time in the supermarket by using crowd-sourced
                   aisle information.
@@ -80,6 +86,86 @@ export default class Projects extends React.Component {
                 <p>
                   Find the nearest grocery store to associate with your shopping
                   list. Just start entering your items and you're ready to shop.
+                </p>
+              </div>
+            </section>
+          </Carousel>
+          <p className="made-with">Made with:</p>
+          <ul className="project-stack">
+            <li className="tech-card">
+              <img src={react} alt="react logo" className="logo32" />
+              <span>React</span>
+            </li>
+            <li className="tech-card">
+              <img src={redux} alt="redux logo" className="logo32" />
+              <span>Redux</span>
+            </li>
+            <li className="tech-card">
+              <img src={nodejs} alt="nodejs logo" className="logo200" />
+              <span>Node.js</span>
+            </li>
+            <li className="tech-card">
+              <img src={mongodb} alt="mongodb logo" className="logo200" />
+              <span>mongoDB</span>
+            </li>
+          </ul>
+        </div>
+        <div className="Project-card">
+          <h3>
+            <a
+              className="github-link"
+              title="Inventory Manager App repository"
+              href="https://github.com/thinkful-ei25/sam-inventory-management-client"
+            >
+              Inventory Manager
+            </a>
+            |
+            <a
+              className="app-link"
+              title="Inventory Managemer App"
+              href="https://inventory-management-app-fo76.herokuapp.com"
+            >
+              Live
+            </a>
+          </h3>
+          <Carousel>
+            <section className="hero">
+              <img src={fo76dashboard} alt="Inventory Manager dashboard" />
+              <div className="hero-titles">
+                <h3>Welcome to the Inventory Manager App</h3>
+                <p>
+                  A companion app for Fallout 76, a game made by Bethesda Game
+                  Studios, to help track a user's character inventory.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={fo76add} alt="Adding an item to your inventory" />
+              <div className="hero-titles">
+                <h3>Add items to your character's inventory</h3>
+                <p>
+                  Easily add items to your inventory and keep them organized by
+                  category.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={fo76view} alt="Gocery Items Page" />
+              <div className="hero-titles">
+                <h3>View the details of your items</h3>
+                <p>
+                  Click on an item in your inventory to see more information
+                  about it, giving the user the option to edit or drop the item.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={fo76edit} alt="Gocery Create New List" />
+              <div className="hero-titles">
+                <h3>Edit the items in your inventory</h3>
+                <p>
+                  By editing an item, you can easily change its details or move
+                  it between categories in your inventory.
                 </p>
               </div>
             </section>
