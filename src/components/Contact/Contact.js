0c2fbd414d6c80dc9e61714df8544b8eccd3cdbe
Sam Dalton <spd4nf@virginia.edu>
2019-03-05 19:50:17 -0600

    used formspree for ContactForm

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './Contact.css';
 
+import ContactForm from '../ContactForm';
 export default class Contact extends React.Component {
   render() {
     return (
       <div className="Contact-container">
         <h2>Contact Me</h2>
         <div className="Contact-card">
-          <p>Contact me I guess</p>
+          <ContactForm />
         </div>
       </div>
     );
