51c60d413a25d39d3d8b648bcd41b4451486dfdf
Sam Dalton <spd4nf@virginia.edu>
2019-03-06 13:58:49 -0600

    added blackjack and noteshark

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,20 +17,20 @@ export default class About extends React.Component {
             particuarly investment management.
           </p>
           <p>
-            About my personal interests. I spend way too of my time digesting
-            sports in about any way possible, whether that be watching as many
-            sporting events as feasibly possible or listening to sports radio
-            all day, particuarly <i>SportsRadio 1310 AM & 96.7 FM The Ticket</i>{' '}
-            in DFW. In the fall, my Sundays are dedicated to following my
-            multiple fantasy football teams and the Dallas Cowboys, although I'm
-            too young to even remember any of their Super Bowls. Come every
-            March, my eyes are fixed on college basketball as I follow March
-            Madness and pull for my Virginia Cavaliers... please don't bring up
-            UMBC. I'll argue with any NBA fan that the 2011 Dallas Mavericks
-            championship led by Dirk Nowitzki was the best NBA championship run
-            in recent memory and continues to age like fine wine with the rise
-            of superteams across the league. I watch way too many Luka Dončić
-            highlight tapes while I hope that the Mavs will turn into the next
+            In my free time, I try digesting sports in about any way possible,
+            whether that be watching as many sporting events as feasibly
+            possible or listening to sports radio all day, particuarly{' '}
+            <i>SportsRadio 1310 AM & 96.7 FM The Ticket</i> in DFW. In the fall,
+            my Sundays are dedicated to following my multiple fantasy football
+            teams and the Dallas Cowboys, although I'm too young to even
+            remember any of their Super Bowls. Come every March, my eyes are
+            fixed on college basketball as I follow March Madness and pull for
+            my Virginia Cavaliers... please don't bring up UMBC. I'll argue with
+            any NBA fan that the 2011 Dallas Mavericks championship led by Dirk
+            Nowitzki was the best NBA championship run in recent memory and
+            continues to age like fine wine with the rise of superteams. While
+            the Warriors currently dominate the league, I watch Luka Dončić
+            highlight tapes hoping that the Mavs will turn into the next NBA
             superteam. My other sports teams include the Texas Longhorns, the
             Texas Rangers, and the Dallas Stars.
           </p>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,6 +7,8 @@ import react from '../../images/react.png';
 import redux from '../../images/redux.png';
 import nodejs from '../../images/node-js.png';
 import mongodb from '../../images/mongodb-logo.png';
+import python from '../../images/python.png';
+import pygame from '../../images/pygame.gif';
 
 //gocery
 import gocerylanding from '../../images/Gocery/landing-page.png';
@@ -20,6 +22,16 @@ import fo76add from '../../images/FO76/add-item.png';
 import fo76view from '../../images/FO76/view-item.png';
 import fo76edit from '../../images/FO76/edit-item.png';
 
+//blackjack
+import blackjack1 from '../../images/Blackjack/screenshot-1.png';
+import blackjack2 from '../../images/Blackjack/screenshot-2.png';
+
+//noteshark
+import noteshark1 from '../../images/NoteShark/homepage.png';
+import noteshark2 from '../../images/NoteShark/guess-note.png';
+import noteshark3 from '../../images/NoteShark/tracking-progress.png';
+import noteshark4 from '../../images/NoteShark/correct-note.png';
+
 import Carousel from '../Carousel';
 
 export default class Projects extends React.Component {
@@ -27,7 +39,7 @@ export default class Projects extends React.Component {
     return (
       <div className="Projects-container">
         <h2>My Projects</h2>
-        <div className="Project-card">
+        <div className="Project-card GoCery">
           <h3>
             <a
               className="github-link"
@@ -114,7 +126,7 @@ export default class Projects extends React.Component {
             </li>
           </ul>
         </div>
-        <div className="Project-card">
+        <div className="Project-card FO76">
           <h3>
             <a
               className="github-link"
@@ -158,7 +170,7 @@ export default class Projects extends React.Component {
               </div>
             </section>
             <section className="hero">
-              <img src={fo76view} alt="Gocery Items Page" />
+              <img src={fo76view} alt="Viewing item details" />
               <div className="hero-titles">
                 <h3>View the details of your items</h3>
                 <p>
@@ -168,12 +180,156 @@ export default class Projects extends React.Component {
               </div>
             </section>
             <section className="hero">
-              <img src={fo76edit} alt="Gocery Create New List" />
+              <img src={fo76edit} alt="Editing an item" />
               <div className="hero-titles">
                 <h3>Edit the items in your inventory</h3>
                 <p>
                   By editing an item, you can easily change its details or move
                   it between categories in your inventory.
+                </p>
+              </div>
+            </section>
+          </Carousel>
+          <p className="made-with">Made with:</p>
+          <ul className="project-stack">
+            <li className="tech-card">
+              <img src={react} alt="react logo" className="logo32" />
+              <span>React</span>
+            </li>
+            <li className="tech-card">
+              <img src={redux} alt="redux logo" className="logo32" />
+              <span>Redux</span>
+            </li>
+            <li className="tech-card">
+              <img src={nodejs} alt="nodejs logo" className="logo200" />
+              <span>Node.js</span>
+            </li>
+            <li className="tech-card">
+              <img src={mongodb} alt="mongodb logo" className="logo200" />
+              <span>mongoDB</span>
+            </li>
+          </ul>
+        </div>
+        <div className="Project-card Blackjack">
+          <h3>
+            <a
+              className="github-link"
+              title="Blackjack github link"
+              href="https://github.com/SamDaltonJr/blackjack"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Blackjack
+            </a>
+            |
+            <a
+              className="app-link"
+              title="Download Blackjack app"
+              href="/files/blackjack.zip"
+              download
+            >
+              Live
+            </a>
+          </h3>
+          <Carousel>
+            <section className="hero">
+              <img src={blackjack2} alt="a game of blackjack" />
+              <div className="hero-titles">
+                <h3>Play a basic game of Blackjack</h3>
+                <p>
+                  This Blackjack application lets a user play 1-on-1 with the
+                  dealerand gives the user recommendations based on the user's
+                  card total and the card the dealer is showing.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={blackjack1} alt="highlight blackjack card counter" />
+              <div className="hero-titles">
+                <h3>Keep track of the card count</h3>
+                <p>
+                  The app also features a "count", a system used by card
+                  counters to track the favorability of the deck towards the
+                  user. A positive count means more high value cards remain in
+                  the deck, favoring the user. A negative count means more low
+                  value cards remain in the deck, favoring the dealer.
+                </p>
+              </div>
+            </section>
+          </Carousel>
+          <p className="made-with">Made with:</p>
+          <ul className="project-stack">
+            <li className="tech-card">
+              <img src={python} alt="python logo" className="logo200" />
+              <span>python</span>
+            </li>
+            <li className="tech-card">
+              <img src={pygame} alt="pygame logo" className="logo200" />
+              <span>pygame</span>
+            </li>
+          </ul>
+        </div>
+        <div className="Project-card NoteShark">
+          <h3>
+            <a
+              className="github-link"
+              title="Note Shark github link"
+              href="https://github.com/thinkful-ei25/knotes-client-sam-allister-mischa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NoteShark
+            </a>
+            |
+            <a
+              className="app-link"
+              title="Download Blackjack app"
+              href="https://knotes-client-sam.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live
+            </a>
+          </h3>
+          <Carousel>
+            <section className="hero">
+              <img src={noteshark1} alt="Note Shark homepage" />
+              <div className="hero-titles">
+                <h3>Test your music knowledge with NoteShark</h3>
+                <p>
+                  NoteShark tests your basic ability to read basic notes using a
+                  spaced repetition algorithm to make sure you truly know your
+                  stuff.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={noteshark2} alt="testing your note knowledge" />
+              <div className="hero-titles">
+                <h3>Answer the correct note</h3>
+                <p>
+                  NoteShark will present you with an image of a note and a
+                  button where you can click to play the sound of the note.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={noteshark3} alt="tracking your answers" />
+              <div className="hero-titles">
+                <h3>Keep track of your progress</h3>
+                <p>
+                  The app tracks your answers and lets you know which notes
+                  you're hitting and which notes you keep missing.
+                </p>
+              </div>
+            </section>
+            <section className="hero">
+              <img src={noteshark4} alt="correct answer" />
+              <div className="hero-titles">
+                <h3>You'll be jamming in no time!</h3>
+                <p>
+                  With NoteShark, you'll get in the groove and master these
+                  basic musical notes.
                 </p>
               </div>
             </section>
